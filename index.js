46c984fbc50098173766606e51d39850159dccb1
Josh Frank <eurosewusa@gmail.com>
2021-06-22 16:01:02 -0400

    models, relationships, migrations and seeders created

---
+++ index.js
@@ -0,0 +1,12 @@
+require( "dotenv" ).config();
+const express = require( "express" );
+const cors = require( "cors" );
+const app = express();
+
+app.use( express.json() );
+app.use( express.urlencoded( { extended: true } ) );
+app.use( cors( { origin: `http://localhost:${ process.env.PORT }` } ) );
+
+app.get( "/", ( request, response ) => response.send( "Test" ) );
+
+app.listen( process.env.PORT, () => console.log( `Listening: port ${ process.env.PORT }` ) );
