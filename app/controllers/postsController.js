3d1c6310271b6135b9c004d47ab30278cac83bd2
Josh Frank <eurosewusa@gmail.com>
2021-07-28 22:14:19 -0400

    last commit before merging

--- app/controllers/postsController.js
+++ app/controllers/postsController.js
@@ -2,9 +2,21 @@ const Post = require('../models').Post;
 
 exports.show = ( request, response ) => {
     return Post.findByPk( request.params.postId, {} )
-        .then( post => {
-            if ( !post ) { response.status( 404 ).send( { error: "Post not found" } ); }
-            else { response.status( 200 ).send( post ); }
-        } )
-        .catch( error => response.status( 400 ).send( error ) );
+    .then( post => {
+        if ( !post ) { response.status( 404 ).send( { error: "Post not found" } ); }
+        else { response.status( 200 ).send( post ); }
+    } )
+    .catch( error => response.status( 400 ).send( error ) );
+}
+
+exports.create = async ( request, response ) => {
+    return await Post.create( {
+        title: request.body.title,
+        content: request.body.content,
+        userId: request.userId
+    }, {} )
+        .then( newPost => Post.findByPk( newPost.id, {} )
+            .then( newPost => response.status( 201 ).send( newPost ) )
+            .catch( error => response.status( 400 ).send( error ) )
+        );
 }
--- app/routes/postRoutes.js
+++ app/routes/postRoutes.js
@@ -1,5 +1,7 @@
+const { authorizeJwt } = require( "../middleware" );
 const postsController = require( "../controllers" ).posts;
 
 module.exports = app => {
     app.get( "/posts/:postId", postsController.show );
+    app.post( "/posts", [ authorizeJwt.verifyToken ], postsController.create );
 };
